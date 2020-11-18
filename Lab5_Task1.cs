fb4eff0851fd3b763d4675e7cc946103471c8aae
Vsevolod Zhdanov <zhdanoov777@gmail.com>
2020-11-18 17:06:46 +0200

    Task 1 for Lab 5

---
+++ Lab5_Task1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Practise
+{
+    //custom collection class
+    class CollectionType<T>
+    {
+        //stores elements
+        private List<T> collection;
+        //readonly number of elements
+        public int Count
+        {
+            get { return collection.Count; }
+        }
+        //indexer
+        public T this[int i]
+        {
+            get { return collection[i]; }
+            set { collection[i] = value; }
+        }
+        //default constructor
+        public CollectionType()
+        {
+            collection = new List<T>();
+        }
+        //constructor with one element as parameter
+        public CollectionType(T element)
+        {
+            collection = new List<T>();
+            collection.Add(element);
+        }
+        //constructor with array as parameter
+        public CollectionType(T[] array)
+        {
+            collection = new List<T>(array);
+        }
+        //method to add element to the collection
+        public void Add(T element)
+        {
+            collection.Add(element);
+        }
+        //method to remove element to the collection
+        public void Remove(T element)
+        {
+            collection.Remove(element);
+        }
+        //method to check if collection contains such element
+        public bool Contains(T element)
+        {
+            return collection.Contains(element);
+        }
+
+    } 
+    
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            CollectionType<int> test = new CollectionType<int>();
+            test.Add(240);
+            Console.Write(test[0]);
+        }
+    }
+}
