04b5b72db24ee527f986c37a818e1e1b0993abba
Vsevolod Zhdanov <zhdanoov777@gmail.com>
2020-12-17 20:56:30 +0200

    Changed function to bool for Unit-testing

--- Lab7.cs
+++ Lab7.cs
@@ -12,18 +12,12 @@ namespace Fun
             Console.WriteLine("Command line argument: /h - help, /f 'filename' - check from file, /s - read strings from CLI");
         }
 
-        void CheckArray(string[] arguments)
+        bool CheckString(string str)
         {
             //Vehicle Registration Number
             Regex VRN = new Regex("^[ABCEHIKMOPTX]{2}(?!0{4})[0-9]{4}[ABCEHIKMOPTX]{2}$");
-            foreach(string str in arguments)
-            {
-                Match match = VRN.Match(str);
-                if (match.Success)
-                    Console.WriteLine($"{str} is a valid VRN");
-                else
-                    Console.WriteLine($"{str} is not a valid VRN");
-            }
+            Match match = VRN.Match(str);
+            return match.Success;
         }
 
         static void Main(string[] args)
@@ -34,27 +28,50 @@ namespace Fun
                 if (args[0] == "/?" || args[0] == "/h")
                     program.PrintHelp();
                 if (args[0] == "/s")
-                    program.CheckArray(args);
-                if (args[0] == "/f")
                 {
-                    using (StreamReader sr = new StreamReader(args[1]))
+                    if (args.Length > 1)
                     {
-                        string[] file_input = sr.ReadToEnd().Split(" ");
-                        program.CheckArray(file_input);
+                        for (int i = 1; i < args.Length; i++)
+                        {
+                            if (program.CheckString(args[i]))
+                                Console.WriteLine($"{args[i]} is a valid VRN");
+                            else
+                                Console.WriteLine($"{args[i]} is not a valid VRN");
+                        }
+                    }
+                    if (args[0] == "/f")
+                    {
+                        using (StreamReader sr = new StreamReader(args[1]))
+                        {
+                            string[] file_input = sr.ReadToEnd().Split(" ");
+                            foreach (string str in file_input)
+                            {
+                                if (program.CheckString(str))
+                                    Console.WriteLine($"{str} is a valid VRN");
+                                else
+                                    Console.WriteLine($"{str} is not a valid VRN");
+                            }
+                        }
                     }
                 }
             }
             else
             {
                 Console.WriteLine("You can input 'end' to stop the program");
-                for(; ; )
+                for (; ; )
                 {
                     Console.WriteLine("Input a string: ");
                     string input = Console.ReadLine();
                     if (input == "end")
                         break;
                     string[] array = input.Split(" ");
-                    program.CheckArray(array);
+                    foreach (string str in array)
+                    {
+                        if (program.CheckString(str))
+                            Console.WriteLine($"{str} is a valid VRN");
+                        else
+                            Console.WriteLine($"{str} is not a valid VRN");
+                    }
                 }
             }
         }
