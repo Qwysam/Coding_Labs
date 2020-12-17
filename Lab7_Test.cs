8dcee581b8a8c439feccc526f46cc941ce064e9b
Vsevolod Zhdanov <61472372+Qwysam@users.noreply.github.com>
2020-12-17 23:32:28 +0200

    Unit tests for Lab7

---
+++ Lab7_Test.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Lab7;
+namespace Lab7Test
+{
+    [TestClass]
+    public class RegeExTest
+    {
+        [TestMethod]
+        public void CheckStringTrue()
+        {
+            Program p = new Program();
+            string input = "BA8167AK";
+            Assert.AreEqual(true,p.CheckString(input));
+        }
+        [TestMethod]
+        public void CheckStringFalse()
+        {
+            Program p = new Program();
+            string input = "BZ8167AK";
+            Assert.AreEqual(false, p.CheckString(input));
+        }
+    }
+}
